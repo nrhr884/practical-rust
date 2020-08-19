f18bde45190e9e39bea42bbbfe8ca95f32f9320b
Naruhiro Hayashi <naruhiro_hayashi@so-netmedia.jp>
2020-08-19 21:39:17 +0900

    add test generator

--- bitonic-sorter/src/third.rs
+++ bitonic-sorter/src/third.rs
@@ -64,6 +64,7 @@ mod tests{
     use super::sort;
     use super::sort_by;
     use crate::SortOrder::*;
+    use crate::utils::{new_u32_vec, is_sorted_ascending, is_sorted_descending};
 
     #[derive(Debug, PartialEq)]
     struct Student {
@@ -148,5 +149,19 @@ mod tests{
         );
 
         assert_eq!(x, expected);
+    }
+
+    #[test]
+    fn sort_u32_large() {
+        {
+            let mut x = new_u32_vec(65536);
+            assert_eq!(sort(&mut x, &Ascending), Ok(()));
+            assert!(is_sorted_ascending(&x));
+        }
+        {
+            let mut x = new_u32_vec(65536);
+            assert_eq!(sort(&mut x, &Descending), Ok(()));
+            assert!(is_sorted_descending(&x));
+        }
     }
 }
